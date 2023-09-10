a7f5e116e7c7981a7a54578acc5b2d0760ffbec6
ayobami <ayobami_adejumo@yahoo.com>
2023-09-10 11:56:45 -0700

    adding initial file

---
+++ Go/go.mod
@@ -0,0 +1,3 @@
+module github.com/janto-pee/golang-dsa
+
+go 1.20
---
+++ Javascript/Saturday09.js
@@ -0,0 +1,41 @@
+// Given a string, find the length of the longest substring in it with no more than K distinct characters.
+
+// Example 1:
+
+// Input: String="araaci", K=2
+// Output: 4
+// Explanation: The longest substring with no more than '2' distinct characters is "araa".
+
+
+function longest_substring_with_k_distinct(str, K) {
+    let windowStart = 0,
+      maxLength = 0,
+      charFrequency = {};
+  
+    // in the following loop we'll try to extend the range [window_start, window_end]
+    for (let windowEnd = 0; windowEnd < str.length; windowEnd++) {
+      const rightChar = str[windowEnd];
+      console.log(rightChar)
+      
+      if (!(rightChar in charFrequency)) {
+        charFrequency[rightChar] = 0;
+      }
+      console.log(charFrequency)
+
+        while(Object.keys(charFrequency).length > K){
+          const leftChar = str[windowStart]
+          charFrequency[leftChar] -= 1
+          if (charFrequency[leftChar] === 0){
+              delete charFrequency[leftChar]
+          }
+          windowStart += 1
+      }
+      maxLength = Math.max(maxLength, windowEnd - windowStart + 1)
+      }
+      return maxLength
+  }
+console.log(longest_substring_with_k_distinct("araaci", 2))
+// longest_substring_with_k_distinct("araaci", 1)
+// longest_substring_with_k_distinct("araaci", 3)
+
+console.log(Object.keys({ a: 0, r: 0, c: 0, i: 0 }).length)
